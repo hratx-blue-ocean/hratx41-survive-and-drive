911f9188b1730aa2f4482a930a6a246c464ea76a
Brent Rusnell <blrusnell@gmail.com>
2019-06-22 14:16:41 -0500

    setting up node server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const createError = require('http-errors');
 const logger = require('morgan');
 const express = require('express');
 const app = express();
+const port = 8000
+
 
 
 // open up CORS 
@@ -13,24 +15,32 @@ app.use((_, res, next) => {
 
 app.use(logger('dev'));
 
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
 // You can place your routes here, feel free to refactor:
-const { example } = require('./routes');
-app.use('/api/example', example)
+// const { example } = require('./routes');
+// app.use('/api/example', example)
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    next(createError(404));
-});
+// // catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+//     next(createError(404));
+// });
 
-// error handler
-app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+// // error handler
+// app.use(function (err, req, res, next) {
+//     // set locals, only providing error in development
+//     res.locals.message = err.message;
+//     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
+//     // render the error page
+//     res.status(err.status || 500);
+//     res.render('error');
+// });
 
 module.exports = app;
