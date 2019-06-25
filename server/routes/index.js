d6dcd022cbdee6de835b9abc398e5b63af269295
Crew Spence <csspence123@gmail.com>
2019-06-25 15:21:52 -0500

    Completed boilerplate for first 7 server requests.

--- server/routes/index.js
+++ server/routes/index.js
@@ -25,16 +25,85 @@ app.post('/', (req, res) => {
 })
 
 //get appointment
+app.get('/', (req, res) => {
+    db.getAppointment(req.body.appointmentId, (err, items) => {
+        if (err) {
+            console.log(`Error finding items w/ category ${req.body.appointmentId}:`);
+            res.status(401).send(err);
+        } else {
+            console.log(`Success! Found results for ${req.body.appointmentId}.`);
+            res.status(201).send(items);
+        }
+    });
+});
 
-//update appointment
+//update appointment return driver
+app.put('/', (req, res) => {
+    db.updateAppointmentReturnDriver(req.body.appointmentId, req.body.driverId, (err, items) => {
+        if(err) { 
+            console.log('Error updating return driver.');
+            res.status(401).send(err);
+        } else {
+            console.log(`Success.  Updated return driver for appointment ${req.body.appointmentId}`);
+            res.status(201).send(items);
+        }
+    })
+  })
+
+//update appointment destination driver
+app.put('/', (req, res) => {
+    db.updateAppointmentReturnDriver(req.body.appointmentId, req.body.driverId, (err, items) => {
+        if(err) { 
+            console.log('Error updating return driver.');
+            res.status(401).send(err);
+        } else {
+            console.log(`Success.  Updated return driver for appointment ${req.body.appointmentId}`);
+            res.status(201).send(items);
+        }
+    })
+  })
 
 //delete appointment
+app.delete('/', (req, res) => {
+    db.deleteAppointment(req.body.appointmentId, (err, items) => {
+      if(err) {
+          console.log(`Error deleting appointment: ${req.body.appointmentId}.`);
+          res.status(401).send(err);
+      } else {
+          console.log(`Success.  Deleted appointment: ${req.body.appointmentId}.`);
+          res.status(201).send(items);
+      }
+    })
+  })
 
 //add driver
+app.post('/', (req, res) => {
+    db.addDriver(req.body, (err, items) => {
+        if(err) {
+          console.log('There was an error invoking app.post to add a driver.');
+          res.status(401).send(err);
+        } else {
+          console.log('Success! Added driver.');
+          res.status(201).send(items);
+        }
+    })
+  })
 
 //get driver
+app.get('/', (req, res) => {
+    db.getDriver(req.body.driverId, (err, items) => {
+        if (err) {
+            console.log(`Error finding driver: ${req.body.driverId}:`);
+            res.status(401).send(err);
+        } else {
+            console.log(`Success! Found driver: ${req.body.driverId}.`);
+            res.status(201).send(items);
+        }
+    });
+});
 
 //update driver
+
 
 //delete driver
 
